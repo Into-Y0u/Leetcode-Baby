9a46c32dfbebe6393560372894a7c66e5d80173b
Arghya <68708178+Into-Y0u@users.noreply.github.com>
2022-02-08 23:23:11 +0530

    Update 300. Longest Increasing Subsequence.py

--- DP/300. Longest Increasing Subsequence.py
+++ DP/300. Longest Increasing Subsequence.py
@@ -4,6 +4,18 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         dp = [1 for i in range(len(nums))]
+        for i in range( len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j] :
+                    dp[i] = max(dp[i] , 1 + dp[j])
+        print(dp)
+        return max(dp)
+    
+    
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [1 for i in range(len(nums))]
         for i in range(len(nums)-2,-1,-1):
             for j in range(i+1,len(nums)):
                 if nums[i] < nums[j] :
