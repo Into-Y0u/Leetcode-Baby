f23d449d6bac188d7610e6474a877bc240c71798
Arghya <68708178+Into-Y0u@users.noreply.github.com>
2022-02-10 17:22:42 +0530

    Create 2. Bineary Search.py

---
+++ Leetcode_Card/Bineary Search/2. Bineary Search.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 1 and nums[0] == target :
+            return 0
+        l,r = 0, len(nums)-1
+        while l <= r :
+            mid = l + (r-l)//2
+            if nums[mid] == target :
+                return mid
+            elif nums[mid] > target:
+                r = mid-1
+            else :
+                l = mid+1
+        return -1
+        
