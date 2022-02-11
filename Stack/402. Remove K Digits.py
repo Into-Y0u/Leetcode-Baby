fe4f5147a7fe963408bf71b14ed9d79d32a1b209
Arghya <68708178+Into-Y0u@users.noreply.github.com>
2022-02-11 12:13:52 +0530

    Update 402. Remove K Digits.py

--- Stack/402. Remove K Digits.py
+++ Stack/402. Remove K Digits.py
@@ -24,3 +24,20 @@ class Solution:
         else :
             return "".join(st)
             
+
+            
+            
+            
+#python special
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack = []
+        for n in num:
+            while k > 0 and stack and stack[-1] > n:
+                stack.pop()
+                k -= 1
+            stack.append(n)
+        
+        ans = stack[:-k] if k else stack
+        return "".join(ans).lstrip('0') or "0"
