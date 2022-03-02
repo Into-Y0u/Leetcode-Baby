21e8071752460cbe251ddc59462035854dbe8e0d
Arghya <68708178+Into-Y0u@users.noreply.github.com>
2022-03-02 11:22:52 +0530

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,10 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dicu = dict()
-        for i,n in enumerate(nums):
-            temp  = target - n 
-            if dicu.get(temp) :
-                if dicu[temp][0] != i :
-                    return [dicu[temp][0] , i] 
-            dicu[n] = [i]
-            
+        dicu = {}
+        for i, n in enumerate(nums):
+            if (target-n) in dicu :
+                return [dicu[target-n] , i ]
+            else :
+                dicu[n] = i
+        
