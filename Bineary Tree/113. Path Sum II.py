0355d15a82a76aa7f0eb3a3b9e3bb4e73bb2854c
Arghya <68708178+Into-Y0u@users.noreply.github.com>
2022-04-19 11:38:43 +0530

    Update 113. Path Sum II.py

--- Bineary Tree/113. Path Sum II.py
+++ Bineary Tree/113. Path Sum II.py
@@ -17,3 +17,40 @@ class Solution:
             
         return dfs(root,0,[])
         
+
+        
+#java Solve
+
+class Solution {
+    
+    List<List<Integer>> paths = new ArrayList<>();
+    
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        
+        if(root == null)
+            return paths;
+        
+        dfs(root, targetSum, new ArrayList<>());
+        
+        return paths;
+    }
+    
+    void dfs(TreeNode node, int targetSum, ArrayList<Integer> path) {
+        
+        // 1- Check specific case
+        if(node == null)
+            return;
+        
+        // 2- Prosses a cell
+            path.add(node.val);
+            targetSum -= node.val;
+        
+        if(targetSum == 0 && node.left == null && node.right == null)
+            paths.add(path);
+        
+        
+        // 3- Call dfs as many times as nedded
+        dfs(node.left, targetSum, new ArrayList<>(path));
+        dfs(node.right, targetSum, new ArrayList<>(path));
+    }
+}
