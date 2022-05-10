64c1f39940039beabbfaed1edcaa6914a933c8aa
Arghya <68708178+Into-Y0u@users.noreply.github.com>
2022-05-10 11:12:35 +0530

    Update 216. Combination Sum III.py

--- Backtrack/216. Combination Sum III.py
+++ Backtrack/216. Combination Sum III.py
@@ -1,29 +1,30 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        arr = [i for i in range(1,10)] 
-        target = n
-        res = []
-  
+        if k*(k+1)//2 < 0 :
+            return []
+        elif k*(k+1)//2 == 0 :
+            return [i for i in range(1,n+1)] 
         
-        def helper(i,cur,target) :
-            if target == 0  :
-                res.append(cur.copy())
+        res = []
+        
+        def helper(cur,arr,t):
+            if t == 0 :
+                if len(cur) == k :
+                    res.append(cur)
+                else :
+                    return 
+            elif t < 0 :
                 return 
-            if target < 0 :
-                return 
+            if len(cur) == k :
+                if t>0 :
+                    return
             
-            for j in range(i,9) :
-                
-                cur.append(arr[j])
-                helper(j+1 , cur , target - arr[j])
-                cur.pop()
+            for i,ele in enumerate(arr):
+                helper(cur+[ele],arr[i+1:],t-ele)
             
-        helper(0,[],target)
-        # print(res)
-        i = 0
-        while i < len(res) and res :
-            if len(res[i])  == k :
-                i+=1
-            else :
-                res.pop(i)
+            
+            
+            
+        helper([],list(range(1,10)),n)
         return res
+        
