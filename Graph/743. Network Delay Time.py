888806276919db68651326deee38c99c57c1d116
Arghya <68708178+Into-Y0u@users.noreply.github.com>
2022-05-14 18:31:57 +0530

    Update 743. Network Delay Time.py

--- Graph/743. Network Delay Time.py
+++ Graph/743. Network Delay Time.py
@@ -25,3 +25,30 @@ class Solution:
             return res
         else :
             return -1
+
+        
+        #using heap 
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        
+        graph = defaultdict(list)
+        
+        for u,v,w in times :
+            graph[u].append((w,v))
+        
+        arr = [(0,k)]
+        vis = set()
+        
+        while arr :
+            time , src = heapq.heappop(arr)
+            vis.add(src)
+            
+            if len(vis) == n :
+                return time 
+            
+            for temp,target in graph[src]:
+                if target not in vis :
+                    heapq.heappush(arr,(time+temp , target))
+
+        return -1
+            
